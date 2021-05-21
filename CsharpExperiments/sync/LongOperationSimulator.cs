a79e72869a3e05632565ed5efddb58885539f54e
ya-ty <61456449+ya-ty@users.noreply.github.com>
2021-05-21 17:18:05 +0530

    added long running operation simulator

--- CsharpExperiments/sync/LongOperationSimulator.cs
+++ CsharpExperiments/sync/LongOperationSimulator.cs
@@ -2,8 +2,9 @@ public class LongOperationSimulator{
     public static void KeepRunningUntil(int seconds){
         var beginTime = DateTime.Now;
         while(true){
-            if((DateTime.Now - beginTime).ToSeconds() > seconds) break;
+            if(System.DateTime.Now.Subtract(beginTime).ToSeconds() > seconds) break;
             sleep(100); //sleep for 100 milliseconds
+            
         }
     }    
 
